6ce7caa9b1db9a45f87a2bd757a8d426e4be656e
Richard Layman <rlayman2000@yahoo.com>
2016-10-19 13:25:33 -0400

    added support for all types except arrays when opening or saving feather files

--- io/src/feather.cpp
+++ io/src/feather.cpp
@@ -27,6 +27,11 @@
 
 using namespace feather;
 
+// TODO - add support for arrays
+// Currently fields that store array values can not be save or loaded.
+// This means that KeyTracks can not save keys and needs to be fixed.
+
+
 bool io::feather_format::open(std::string filename) {
     status p;
 
@@ -92,11 +97,29 @@ bool io::feather_format::open(std::string filename) {
                 << "\tlength:" << data.length << std::endl;
             // set the field value
             field::FieldBase* srcfield = plugin::get_node_field_base(node.uid,node.nid,data.fid);
+
+            bool bval;
             int ival;
             float fval;
             double dval;
-               
+            FVertex3D vertexval;
+            FVector3D vectorval;
+            FMesh meshval;
+            FColorRGB rgbval;
+            FColorRGBA rgbaval;
+            FMatrix3x3 matrix3x3val;
+            FMatrix4x4 matrix4x4val;
+            FVertexIndiceWeight vertexindiceweightval;
+            FVertexIndiceGroupWeight vertexindicegroupweightval;
+            FKey keyval;
+            FCurvePoint2D curvepoint2dval;
+            FCurvePoint3D curvepoint3dval;
+
             switch(srcfield->type){
+                case field::Bool:
+                    file.read((char*)&bval,sizeof(bool));
+                    static_cast<field::Field<bool>*>(srcfield)->value = bval;
+                    break;
                 case field::Int:
                     file.read((char*)&ival,sizeof(int));
                     static_cast<field::Field<int>*>(srcfield)->value = ival;
@@ -112,6 +135,54 @@ bool io::feather_format::open(std::string filename) {
                 case field::Real:
                     file.read((char*)&dval,sizeof(double));
                     static_cast<field::Field<double>*>(srcfield)->value = dval;
+                    break;
+                case field::Vertex:
+                    file.read((char*)&vertexval,sizeof(FVertex3D));
+                    static_cast<field::Field<FVertex3D>*>(srcfield)->value = vertexval;
+                    break;
+                case field::Vector:
+                    file.read((char*)&vectorval,sizeof(FVector3D));
+                    static_cast<field::Field<FVector3D>*>(srcfield)->value = vectorval;
+                    break;
+                case field::Mesh:
+                    file.read((char*)&meshval,sizeof(FMesh));
+                    static_cast<field::Field<FMesh>*>(srcfield)->value = meshval;
+                    break;
+                case field::RGB:
+                    file.read((char*)&rgbval,sizeof(FColorRGB));
+                    static_cast<field::Field<FColorRGB>*>(srcfield)->value = rgbval;
+                    break;
+                case field::RGBA:
+                    file.read((char*)&rgbaval,sizeof(FColorRGBA));
+                    static_cast<field::Field<FColorRGBA>*>(srcfield)->value = rgbaval;
+                    break;
+                case field::Matrix3x3:
+                    file.read((char*)&matrix3x3val,sizeof(FMatrix3x3));
+                    static_cast<field::Field<FMatrix3x3>*>(srcfield)->value = matrix3x3val;
+                    break;
+                case field::Matrix4x4:
+                    file.read((char*)&matrix4x4val,sizeof(FMatrix4x4));
+                    static_cast<field::Field<FMatrix4x4>*>(srcfield)->value = matrix4x4val;
+                    break;
+                case field::VertexIndiceWeight:
+                    file.read((char*)&vertexindiceweightval,sizeof(FVertexIndiceWeight));
+                    static_cast<field::Field<FVertexIndiceWeight>*>(srcfield)->value = vertexindiceweightval;
+                    break;
+                case field::VertexIndiceGroupWeight:
+                    file.read((char*)&vertexindicegroupweightval,sizeof(FVertexIndiceGroupWeight));
+                    static_cast<field::Field<FVertexIndiceGroupWeight>*>(srcfield)->value = vertexindicegroupweightval;
+                    break;
+                case field::Key:
+                    file.read((char*)&keyval,sizeof(FKey));
+                    static_cast<field::Field<FKey>*>(srcfield)->value = keyval;
+                    break;
+                case field::CurvePoint2D:
+                    file.read((char*)&curvepoint2dval,sizeof(FCurvePoint2D));
+                    static_cast<field::Field<FCurvePoint2D>*>(srcfield)->value = curvepoint2dval;
+                    break;
+                case field::CurvePoint3D:
+                    file.read((char*)&curvepoint3dval,sizeof(FCurvePoint3D));
+                    static_cast<field::Field<FCurvePoint3D>*>(srcfield)->value = curvepoint3dval;
                     break;
                     // field::Node can be skipped since it only for connections
                 default:
@@ -199,6 +270,9 @@ bool io::feather_format::save(std::string filename) {
                 fieldinfo.fid = fid;
                 fieldinfo.type = srcfield->type;
                 switch(srcfield->type){
+                    case field::Bool:
+                        fieldinfo.length = sizeof(bool);
+                        break;
                     case field::Int:
                         fieldinfo.length = sizeof(int);
                         break;
@@ -211,6 +285,42 @@ bool io::feather_format::save(std::string filename) {
                     case field::Real:
                         fieldinfo.length = sizeof(double);
                         break;
+                    case field::Vertex:
+                        fieldinfo.length = sizeof(FVertex3D);
+                        break;
+                    case field::Vector:
+                        fieldinfo.length = sizeof(FVector3D);
+                        break;
+                    case field::Mesh:
+                        fieldinfo.length = sizeof(FMesh);
+                        break;
+                    case field::RGB:
+                        fieldinfo.length = sizeof(FColorRGB);
+                        break;
+                    case field::RGBA:
+                        fieldinfo.length = sizeof(FColorRGBA);
+                        break;
+                    case field::Matrix3x3:
+                        fieldinfo.length = sizeof(FMatrix3x3);
+                        break;
+                    case field::Matrix4x4:
+                        fieldinfo.length = sizeof(FMatrix4x4);
+                        break;
+                    case field::VertexIndiceWeight:
+                        fieldinfo.length = sizeof(FVertexIndiceWeight);
+                        break;
+                    case field::VertexIndiceGroupWeight:
+                        fieldinfo.length = sizeof(FVertexIndiceGroupWeight);
+                        break;
+                    case field::Key:
+                        fieldinfo.length = sizeof(FKey);
+                        break;
+                    case field::CurvePoint2D:
+                        fieldinfo.length = sizeof(FCurvePoint2D);
+                        break;
+                    case field::CurvePoint3D:
+                        fieldinfo.length = sizeof(FCurvePoint3D);
+                        break;
                     default:
                         std::cout << "FAILED TO FIND VALUE TYPE!\n";
                         fieldinfo.length = 0;
@@ -221,7 +331,20 @@ bool io::feather_format::save(std::string filename) {
                 if( fieldinfo.type == field::Int ||
                         fieldinfo.type == field::Float ||
                         fieldinfo.type == field::Double ||
-                        fieldinfo.type == field::Real )
+                        fieldinfo.type == field::Real ||
+                         fieldinfo.type == field::Vertex ||
+                         fieldinfo.type == field::Vector ||
+                        fieldinfo.type == field::Mesh ||
+                        fieldinfo.type == field::RGB ||
+                        fieldinfo.type == field::RGBA ||
+                        fieldinfo.type == field::Matrix3x3 ||
+                        fieldinfo.type == field::Matrix4x4 ||
+                        fieldinfo.type == field::VertexIndiceWeight ||
+                        fieldinfo.type == field::VertexIndiceGroupWeight ||
+                        fieldinfo.type == field::Key ||
+                        fieldinfo.type == field::CurvePoint2D ||
+                        fieldinfo.type == field::CurvePoint3D
+                  )
                     fielddata.push_back(fieldinfo);
 
             } 
@@ -253,10 +376,29 @@ bool io::feather_format::save(std::string filename) {
         for ( data_t fieldinfo : fielddata ) {
             field::FieldBase* srcfield = plugin::get_node_field_base(uid,node.nid,fieldinfo.fid);
             file.write((char*)&fieldinfo,sizeof(data_t));
-            unsigned int ival;
+
+            bool bval;
+            int ival;
             float fval;
             double dval;
+            FVertex3D vertexval;
+            FVector3D vectorval;
+            FMesh meshval;
+            FColorRGB rgbval;
+            FColorRGBA rgbaval;
+            FMatrix3x3 matrix3x3val;
+            FMatrix4x4 matrix4x4val;
+            FVertexIndiceWeight vertexindiceweightval;
+            FVertexIndiceGroupWeight vertexindicegroupweightval;
+            FKey keyval;
+            FCurvePoint2D curvepoint2dval;
+            FCurvePoint3D curvepoint3dval;
+
             switch(srcfield->type){
+                case field::Bool:
+                    bval = static_cast<field::Field<bool>*>(srcfield)->value;
+                    file.write((char*)&bval,fieldinfo.length);
+                    break;
                 case field::Int:
                     ival = static_cast<field::Field<int>*>(srcfield)->value;
                     file.write((char*)&ival,fieldinfo.length);
@@ -272,6 +414,54 @@ bool io::feather_format::save(std::string filename) {
                 case field::Real:
                     dval = static_cast<field::Field<double>*>(srcfield)->value;
                     file.write((char*)&dval,fieldinfo.length);
+                    break;
+                case field::Vertex:
+                    vertexval = static_cast<field::Field<FVertex3D>*>(srcfield)->value;
+                    file.write((char*)&vertexval,fieldinfo.length);
+                    break;
+                case field::Vector:
+                    vectorval = static_cast<field::Field<FVector3D>*>(srcfield)->value;
+                    file.write((char*)&vectorval,fieldinfo.length);
+                    break;
+                case field::Mesh:
+                    meshval = static_cast<field::Field<FMesh>*>(srcfield)->value;
+                    file.write((char*)&meshval,fieldinfo.length);
+                    break;
+                case field::RGB:
+                    rgbval = static_cast<field::Field<FColorRGB>*>(srcfield)->value;
+                    file.write((char*)&rgbval,fieldinfo.length);
+                    break;
+                case field::RGBA:
+                    rgbaval = static_cast<field::Field<FColorRGBA>*>(srcfield)->value;
+                    file.write((char*)&rgbaval,fieldinfo.length);
+                    break;
+                case field::Matrix3x3:
+                    matrix3x3val = static_cast<field::Field<FMatrix3x3>*>(srcfield)->value;
+                    file.write((char*)&matrix3x3val,fieldinfo.length);
+                    break;
+                case field::Matrix4x4:
+                    matrix4x4val = static_cast<field::Field<FMatrix4x4>*>(srcfield)->value;
+                    file.write((char*)&matrix4x4val,fieldinfo.length);
+                    break;
+                case field::VertexIndiceWeight:
+                    vertexindiceweightval = static_cast<field::Field<FVertexIndiceWeight>*>(srcfield)->value;
+                    file.write((char*)&vertexindiceweightval,fieldinfo.length);
+                    break;
+                case field::VertexIndiceGroupWeight:
+                    vertexindicegroupweightval = static_cast<field::Field<FVertexIndiceGroupWeight>*>(srcfield)->value;
+                    file.write((char*)&vertexindicegroupweightval,fieldinfo.length);
+                    break;
+                case field::Key:
+                    keyval = static_cast<field::Field<FKey>*>(srcfield)->value;
+                    file.write((char*)&keyval,fieldinfo.length);
+                    break;
+                case field::CurvePoint2D:
+                    curvepoint2dval = static_cast<field::Field<FCurvePoint2D>*>(srcfield)->value;
+                    file.write((char*)&curvepoint2dval,fieldinfo.length);
+                    break;
+                case field::CurvePoint3D:
+                    curvepoint3dval = static_cast<field::Field<FCurvePoint3D>*>(srcfield)->value;
+                    file.write((char*)&curvepoint3dval,fieldinfo.length);
                     break;
                     // field::Node can be skipped since it only for connections
                 default:
