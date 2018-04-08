308cb3e5f70a2e2724ff10342c56f70f4f9e0eb0
Richard Layman <rlayman2000@yahoo.com>
2018-04-08 09:57:00 -0400

    lux plugin can now load scene objects and render them

--- lux/src/main.cpp
+++ lux/src/main.cpp
@@ -31,13 +31,13 @@
 #include <feather/parameter.hpp>
 #include <feather/command.hpp>
 #include <feather/scenegraph.hpp>
+
 #include <luxcore/luxcore.h>
 #include <luxrays/utils/properties.h>
 #include <slg/slg.h>
 #include <chrono>
 #include <thread>
-
-
+#include "lux.hpp"
 
 
 /*
@@ -125,6 +125,7 @@ ADD_FIELD_TO_NODE(LUX_SHADER_MATTE,FColorRGBA,field::RGBA,field::connection::In,
 struct LuxRenderProperties {
     luxcore::Scene* scene;
     luxrays::Properties* properties;
+    luxrays::Properties* sceneprops;
     luxcore::RenderConfig* config;
     luxcore::RenderSession* session;
 };
@@ -154,6 +155,11 @@ namespace feather
             sluxprops.properties = nullptr;
         }
 
+        if(sluxprops.sceneprops != nullptr) {
+            delete sluxprops.sceneprops;
+            sluxprops.sceneprops = nullptr;
+        }
+
         if(sluxprops.config != nullptr) {
             delete sluxprops.config;
             sluxprops.config = nullptr;
@@ -166,25 +172,13 @@ namespace feather
 
         luxcore::Init();
 
-        // LOAD THE SCENE
-        //sluxprops.scene = new luxcore::Scene();
+ 
+        //std::cout << "FRAME WIDTH:" << frame_width << " FRAME HEIGHT:" << frame_height << std::endl;
 
-        std::vector<uint32_t> cameras;
-        std::vector<uint32_t> shapes;
+        sluxprops.scene = new luxcore::Scene();
 
-        scenegraph::get_node_by_type(node::Camera,cameras);
-        scenegraph::get_node_by_type(node::Shape,shapes);
-        std::cout << "NUMBER OF CAMERAS:" << cameras.size() << std::endl;
-        std::cout << "NUMBER OF SHAPES:" << shapes.size() << std::endl;
-
-        
-        // TODO - CLEANUP OLD POINTERS HERE
-
-        sluxprops.scene = new luxcore::Scene("/home/richard/luxed_tests/test_set/set.scn");
-        //sluxprops.properties = new luxrays::Properties("/home/richard/luxed_tests/test_set/render.cfg");
-
+        // RENDER PROPERTIES
         sluxprops.properties = new luxrays::Properties();
-
         sluxprops.properties->Set(luxrays::Property("renderengine.type",std::string("PATHOCL")));
         sluxprops.properties->Set(luxrays::Property("opencl.platform.index",-1));
         sluxprops.properties->Set(luxrays::Property("opencl.cpu.use",0));
@@ -201,17 +195,213 @@ namespace feather
         sluxprops.properties->Set(luxrays::Property("film.filter.xwidth",1));
         sluxprops.properties->Set(luxrays::Property("film.filter.ywidth",1));
 
+        // THIS WILL CAUSE ISSUES
         //sluxprops.properties->Set(luxrays::Property("film.imagepipeline.0.type","TONEMAP_LINEAR"));
         //sluxprops.properties->Set(luxrays::Property("film.imagepipeline.1.type","GAMMA_CORRECTION"));
         //sluxprops.properties->Set(luxrays::Property("film.imagepipeline.1.value",2.2));
 
+        // SCENE PROPERTIES
+        sluxprops.sceneprops = new luxrays::Properties();
+ 
+        // CAMERA
+
+        sluxprops.sceneprops->Set(luxrays::Property("scene.camera.lookat.orig",luxrays::PropertyValues{0,1,6}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.camera.lookat.target",luxrays::PropertyValues{0.0,1,0.0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.camera.up", luxrays::PropertyValues{0, 1, 0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.camera.cliphither", 0.001));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.camera.clipyon", 100.00));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.camera.lensradius", 0.00));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.camera.focaldistance", 0.28));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.camera.fieldofview", 49));
+
+        // VOLUME
+
+        sluxprops.sceneprops->Set(luxrays::Property("scene.volumes.default_volume.type",std::string("clear")));
+
+        // LIGHT - You must have at least one light source that is not an emission
+        sluxprops.sceneprops->Set(luxrays::Property("scene.lights.ext_light.gain", luxrays::PropertyValues{0, 0, 0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.lights.ext_light.gain.transformation", luxrays::PropertyValues{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.lights.ext_light.gain.samples", -1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.lights.ext_light.gain.visibility.indirect.diffuse.enable", 1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.lights.ext_light.gain.visibility.indirect.glossy.enable", 1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.lights.ext_light.gain.visibility.indirect.specular.enable", 1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.lights.ext_light.gain.dir", luxrays::PropertyValues{-0.51, -3.38, 0.76}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.lights.ext_light.gain.turbidity", 2.20));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.lights.ext_light.gain.relsize", 1));
+ 
+        // LIGHT MATERIAL
+
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.type",std::string("matte")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.kd",luxrays::PropertyValues{1.0,1.0,1.0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.bumptex",luxrays::PropertyValues{0.0,0.0,0.0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.normaltex",luxrays::PropertyValues{0.0,0.0,0.0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.samples",-1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.visibility.indirect.diffuse.enable",1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.visibility.indirect.glossy.enable",1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.visibility.indirect.specular.enable",1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.bumpsamplingdistance",0.001));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.volume.interior",std::string("default_volume")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.volume.exterior",std::string("default_volume")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.id",0));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.transparency",luxrays::PropertyValues{1.0,1.0,1.0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.shadowcatcher.enable",0));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission",luxrays::PropertyValues{1.0,1.0,1.0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.gain",luxrays::PropertyValues{1.0,1.0,1.0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.power",10.0));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.efficency",90.0));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.mapfile",std::string("")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.gamma",2.2));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.iesfile",std::string("")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.flipz",0));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.samples",-1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.map.width",0));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.map.height",0));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_light_mat.emission.id",0));
+
+
+        // DEFAULT MATERIAL 
+
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.type",std::string("roughmatte")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.kd",luxrays::PropertyValues{0.5,0.5,0.5}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.sigma",luxrays::PropertyValues{0,0,0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.bumptex",luxrays::PropertyValues{0,0,0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.normaltex",luxrays::PropertyValues{0,0,0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.samples",-1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.visibility.indirect.diffuse.enable",1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.visibility.indirect.glossy.enable",1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.visibility.indirect.specular.enable",1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.bumpsamplingdistance",0.001));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.volume.interior",std::string("default_volume")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.volume.exterior",std::string("default_volume")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.default_mat.id",1));
+
+
+        /*
+        luxrays::Point* points = luxcore::Scene::AllocVerticesBuffer(3);
+        luxrays::Triangle* tris = luxcore::Scene::AllocTrianglesBuffer(1);
+        points[0]={0,0,0};
+        points[1]={4,0,0};
+        points[2]={0,0,4};
+        tris[0]={2,1,0};
+        luxrays::ExtTriangleMesh mesh(3,1,points,tris);
+        sluxprops.scene->DefineMesh("test_mesh",&mesh);
+        */
+
+        // LOAD SHAPE NODES
+        std::vector<uint32_t> cameras;
+        std::vector<uint32_t> shapes;
+
+        scenegraph::get_node_by_type(node::Camera,cameras);
+        scenegraph::get_node_by_type(node::Shape,shapes);
+        std::cout << "NUMBER OF CAMERAS:" << cameras.size() << std::endl;
+        std::cout << "NUMBER OF SHAPES:" << shapes.size() << std::endl;
+
+        // load polygons
+        for(auto uid : shapes) {
+            status error;
+            std::string name;
+            scenegraph::get_node_name(uid,name,error);
+            // get the mesh in
+            field::Field<FMesh>* meshfield = static_cast<field::Field<FMesh>*>(scenegraph::get_fieldBase(uid,1));
+            // create lux mesh
+            if(meshfield->value.is_tri_mesh())
+                std::cout << "TRI ONLY MESH\n";
+            else
+                std::cout << "NOT A TRI MESH\n";
+
+            // vertex
+            luxrays::Point* points = luxcore::Scene::AllocVerticesBuffer(meshfield->value.v.size());
+ 
+            //std::vector<luxrays::Point> points;
+            uint32_t i=0;
+            for(auto point : meshfield->value.v) {
+                points[i++] = {point.x,point.y,point.z};
+                //points.push_back(luxrays::Point(point.x,point.y,point.z));
+            }
+
+            // triangle face indics
+
+            std::vector<luxrays::Triangle> tris;
+            i=0;
+            uint32_t fcount=0;
+            for(auto face : meshfield->value.f) {
+                //tris.push_back(luxrays::Triangle(static_cast<uint32_t>(face[0].v),static_cast<uint32_t>(face[1].v),static_cast<uint32_t>(face[3].v)));
+                for(uint32_t j=0; j < face.size(); j++) {
+                    if(j < 3) {
+                        tris.push_back(luxrays::Triangle(static_cast<uint32_t>(face[0].v),static_cast<uint32_t>(face[1].v),static_cast<uint32_t>(face[2].v)));
+                        j+=2;
+                    } else {
+                        tris.push_back(luxrays::Triangle(static_cast<uint32_t>(face[0].v),static_cast<uint32_t>(face[j-1].v),static_cast<uint32_t>(face[j].v)));
+                    } 
+                    fcount++;
+                }
+            }
+            luxrays::Triangle* luxtris = luxcore::Scene::AllocTrianglesBuffer(fcount);
+
+            i=0;
+            for(auto tri : tris) {
+                luxtris[i++] = {tri.v[0],tri.v[1],tri.v[2]};
+            }
+
+            //luxrays::ExtTriangleMesh* mesh = new luxrays::ExtTriangleMesh(meshfield->value.v.size(),tris.size(),static_cast<luxrays::Point*>(&points[0]),static_cast<luxrays::Triangle*>(&tris[0]));
+            luxrays::ExtTriangleMesh* mesh = new luxrays::ExtTriangleMesh(meshfield->value.v.size(),fcount,points,luxtris);
+
+            std::cout << "DEFINING MESH " << name.c_str() << std::endl;
+
+            sluxprops.scene->DefineMesh(name.c_str(),mesh);
+            std::stringstream ss;
+            ss << "scene.objects." << name.c_str() << ".shape";
+            std::cout << ss.str() << std::endl;
+            sluxprops.sceneprops->Set(luxrays::Property(ss.str().c_str(),std::string(name.c_str())));
+            ss.str(std::string());
+            ss << "scene.objects." << name.c_str() << ".material";
+            std::cout << ss.str() << std::endl;
+            if(name == "light_shape")
+                sluxprops.sceneprops->Set(luxrays::Property(ss.str().c_str(),std::string("default_light_mat")));
+            else 
+                sluxprops.sceneprops->Set(luxrays::Property(ss.str().c_str(),std::string("default_mat")));
+       }
+
+
+        /*
+        // LIGHT SETUP
+        sluxprops.sceneprops->Set(luxrays::Property("scene.shapes.light_housing_shp.type",std::string("inlinedmesh")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.shapes.light_housing_shp.vertices",luxrays::PropertyValues{0.0,3.0,0.0, 1.0,3.0,0.0, 0.0,3.0,1.0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.shapes.light_housing_shp.faces",luxrays::PropertyValues{0,1,2}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.objects.light_housing_obj.material",std::string("light_housing_mat")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.objects.light_housing_obj.shape",std::string("light_housing_shp")));
+        
+        // CUBE
+
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.type",std::string("roughmatte")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.kd",luxrays::PropertyValues{0.0,0.75,0.75}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.sigma",luxrays::PropertyValues{0,0,0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.bumptex",luxrays::PropertyValues{0,0,0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.normaltex",luxrays::PropertyValues{0,0,0}));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.samples",-1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.visibility.indirect.diffuse.enable",1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.visibility.indirect.glossy.enable",1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.visibility.indirect.specular.enable",1));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.bumpsamplingdistance",0.001));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.volume.interior",std::string("default_volume")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.volume.exterior",std::string("default_volume")));
+        sluxprops.sceneprops->Set(luxrays::Property("scene.materials.cube_mat.id",1));
+        */
+
+        // CUBE MATERIAL
+        //sluxprops.sceneprops->Set(luxrays::Property("scene.objects.test_mesh.shape",std::string("test_mesh")));
+        //sluxprops.sceneprops->Set(luxrays::Property("scene.objects.test_mesh.material",std::string("cube_mat")));
+
+        sluxprops.scene->Parse(*sluxprops.sceneprops);
         sluxprops.config = new luxcore::RenderConfig(*sluxprops.properties,sluxprops.scene);
         sluxprops.session = new luxcore::RenderSession(sluxprops.config);
 
-        //sluxprops.config->GetProperties().Set(luxrays::Property("frame.width")(frame_width));
-        //sluxprops.config->GetProperties().Set(luxrays::Property("frame.height",luxrays::PropertyValue(frame_height)));
-        //sluxprops.config->GetProperties().Get("frame.height")=34;
- 
+        /* 
+        std::vector<std::string> names = sluxprops.properties->GetAllNames();
+        for(auto name : names)
+            std::cout << "property name:" << name << std::endl;
+        */
+
         sluxprops.session->Start();
 
         return status();
